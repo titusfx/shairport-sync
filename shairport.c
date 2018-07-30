ac8afe0ab2e81c09278cff15da781275a08aa593
Mike Brady <mikebrady@eircom.net>
2018-07-30 10:30:00 +0100

    Remove an incorrect warning about using the "general" "statistics" setting.

--- shairport.c
+++ shairport.c
@@ -464,7 +464,7 @@ int parse_options(int argc, char **argv) {
       }
 
       /* Get the statistics setting. */
-      if (!config_set_lookup_bool(config.cfg, "general.statistics",
+      if (config_set_lookup_bool(config.cfg, "general.statistics",
                                   &(config.statistics_requested))) {
         warn("The \"general\" \"statistics\" setting is deprecated. Please use the \"diagnostics\" "
              "\"statistics\" setting instead.");
